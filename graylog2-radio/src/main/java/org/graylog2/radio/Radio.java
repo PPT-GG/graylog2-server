603b7aad196258508ff837d91011fd6b3b1be840
Dennis Oelkers <dennis@torch.sh>
2014-02-12 18:09:57 +0100

    Adding Factories for Output- and Processbuffers and -processors. Injecting and using them in Core and Radio class.

--- graylog2-radio/src/main/java/org/graylog2/radio/Radio.java
+++ graylog2-radio/src/main/java/org/graylog2/radio/Radio.java
@@ -71,6 +71,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
